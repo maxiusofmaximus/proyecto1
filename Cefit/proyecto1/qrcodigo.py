f033f699a87f1d7b7f996c7a60987565251b8a25
Pedro Abelardo <70430136+maxiusofmaximus@users.noreply.github.com>
2023-09-29 21:11:42 -0500

    Add files via upload

--- Cefit/proyecto1/qrcodigo.py
+++ Cefit/proyecto1/qrcodigo.py
@@ -1,27 +1,39 @@
 # 1. Generador de Códigos QR: Implementa un generador de códigos QR que convierta texto o enlaces en códigos QR.
+import tkinter as tk
+from PIL import Image, ImageTk
 import qrcode
-import tkinter
 
 def generar_codigo_qr(texto, nombre_archivo):
-    # Crea un objeto QRCode
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
         border=4,
     )
-
-    # Agrega el texto o enlace al código QR
     qr.add_data(texto)
     qr.make(fit=True)
 
-    # Crea una imagen del código QR y guárdala en un archivo
     img = qr.make_image(fill_color="black", back_color="white")
     img.save(nombre_archivo)
 
+def mostrar_codigo_qr(texto):
+    ventana = tk.Tk()
+    ventana.title("Código QR")
+
+    # Genera el código QR y lo guarda en un archivo
+    nombre_archivo = "codigo_qr.png"
+    generar_codigo_qr(texto, nombre_archivo)
+
+    # Carga la imagen PNG en un objeto PhotoImage de Tkinter
+    imagen = Image.open(nombre_archivo)
+    foto = ImageTk.PhotoImage(imagen)
+
+    # Crea un Label para mostrar la imagen
+    label_imagen = tk.Label(ventana, image=foto)
+    label_imagen.pack()
+
+    ventana.mainloop()
+
 if __name__ == "__main__":
     texto = input("Ingrese el texto o enlace que desea convertir en código QR: ")
-    nombre_archivo = input("Ingrese el nombre del archivo de imagen de salida (por ejemplo, codigo_qr.png): ")
-
-    generar_codigo_qr(texto, nombre_archivo)
-    print(f"Se ha generado el código QR y se ha guardado en {nombre_archivo}")
+    mostrar_codigo_qr(texto)
